20eebe1ffe5e7633c5851f45d389389cfa11442f
ter5298 <ter5298@gmail.com>
2026-06-14 00:00:05 +0800

    預設動作庫名稱正規化(全形括號)、更新為43動作、SW v14

--- js/seed.js
+++ js/seed.js
@@ -1,46 +1,35 @@
 // 預設資料：新使用者首次開啟（資料庫為空）時自動載入。
-// 由 tools/make-seed.js 從個人資料產生，僅含動作庫與一個範本，不含任何訓練紀錄。
+// 由 tools 從個人資料產生，僅含動作庫與一個範本，不含任何訓練紀錄。
 export const SEED = {
   "exercises": [
     {
-      "id": "122a1a82-f026-4e5e-9e3b-1e8d66b87a6b",
-      "name": "滑輪下拉",
+      "id": "01faa52d-afe9-4576-806d-3a6c3951ba51",
+      "name": "仰臥直臂上拉（啞鈴）",
       "muscles": [
         {
           "muscle": "背",
           "weight": 1
         }
       ],
-      "createdAt": 1781338662648
+      "createdAt": 1781365525810
     },
     {
-      "id": "4f09d26d-c371-4c34-8a9f-585596c869e3",
-      "name": "分動式划船 (機器)",
+      "id": "025e32ac-929e-49a7-a76a-b4c28031b79a",
+      "name": "過頭三頭肌伸展（滑輪）",
       "muscles": [
         {
-          "muscle": "背",
+          "muscle": "三頭",
           "weight": 1
         }
       ],
       "createdAt": 1781338662648
     },
     {
-      "id": "5151ff8b-7abd-4940-b5e9-8cf01e243606",
-      "name": "直臂下壓（滑輪）",
+      "id": "09852e25-10d0-49f8-b9cf-3ca7cb675bf3",
+      "name": "反向飛鳥（機器）",
       "muscles": [
         {
-          "muscle": "背",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "0f6afb63-3b68-4968-8b5b-691173ce8469",
-      "name": "深蹲 (槓鈴)",
-      "muscles": [
-        {
-          "muscle": "腿臀",
+          "muscle": "肩",
           "weight": 1
         }
       ],
@@ -48,18 +37,7 @@ export const SEED = {
     },
     {
       "id": "0d703df4-20cd-4071-950a-c43706787d33",
-      "name": "坐姿雙腿彎舉 (機器)",
-      "muscles": [
-        {
-          "muscle": "腿臀",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "eb05fe5e-491a-4515-a587-5d7714e64167",
-      "name": "腿部外展",
+      "name": "坐姿雙腿彎舉（機器）",
       "muscles": [
         {
           "muscle": "腿臀",
@@ -80,8 +58,8 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "abbde7dd-278e-4ba8-b4c6-2a2a0f86a720",
-      "name": "腿屈伸 (機器)",
+      "id": "0f6afb63-3b68-4968-8b5b-691173ce8469",
+      "name": "深蹲（槓鈴）",
       "muscles": [
         {
           "muscle": "腿臀",
@@ -91,8 +69,52 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "eae33992-5d5b-48a9-94a2-30f95dff142a",
-      "name": "上斜彎舉 (啞鈴)",
+      "id": "102ea501-db99-4e31-bf21-c9b6fa8f2960",
+      "name": "引體向上（輔助）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "10d32d43-b8e4-4949-b8fd-e2852843efbd",
+      "name": "單臂划船（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781365560159
+    },
+    {
+      "id": "122a1a82-f026-4e5e-9e3b-1e8d66b87a6b",
+      "name": "滑輪下拉",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "14647ded-dbf9-4a6e-b8bc-b55203168d53",
+      "name": "坐姿窄握划船（機器）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "26154708-c018-42c5-99b4-3f81083ff029",
+      "name": "牧師椅彎舉（機器）",
       "muscles": [
         {
           "muscle": "二頭",
@@ -102,19 +124,8 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "8d4e4d9b-9c04-42a3-b9ee-36ffa24d0db9",
-      "name": "槌式彎舉 (啞鈴)",
-      "muscles": [
-        {
-          "muscle": "二頭",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "b7212335-963c-4a86-9abd-3614e3bdabe2",
-      "name": "上斜臥推 (槓鈴)",
+      "id": "2fbac25e-fbb0-4edd-a6ea-d10f3692a1b3",
+      "name": "上斜臥推（機器）",
       "muscles": [
         {
           "muscle": "胸",
@@ -124,11 +135,77 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "c9bbbb70-3b87-43d2-962a-34773b8fff48",
-      "name": "肩推 (機器)",
+      "id": "4185b790-e7c3-45b4-b6c3-6a8a9a72b785",
+      "name": "肩推（槓鈴）",
       "muscles": [
         {
           "muscle": "肩",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "4a6628ca-55f6-4b3c-a1cf-bb39256bbe24",
+      "name": "側平舉（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "肩",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "4f09d26d-c371-4c34-8a9f-585596c869e3",
+      "name": "分動式划船（機器）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "5151ff8b-7abd-4940-b5e9-8cf01e243606",
+      "name": "直臂下壓（滑輪）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "5597fee9-59e0-490a-b2aa-36ad5c9b6394",
+      "name": "上斜臥推（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "胸",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "562a9462-05fd-43b7-afe8-2e487acef683",
+      "name": "臥推（槓鈴）",
+      "muscles": [
+        {
+          "muscle": "胸",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "5e1438a9-cba7-4ecf-a311-6a10bc070ba0",
+      "name": "蝴蝶機夾胸（機器）",
+      "muscles": [
+        {
+          "muscle": "胸",
           "weight": 1
         }
       ],
@@ -146,8 +223,52 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "4a6628ca-55f6-4b3c-a1cf-bb39256bbe24",
-      "name": "側平舉 (啞鈴)",
+      "id": "793e511e-58e7-4580-8e26-a9bb4a4bbdf1",
+      "name": "臀推（槓鈴）",
+      "muscles": [
+        {
+          "muscle": "腿臀",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "8567b7aa-405c-45a4-961f-735c9f7d0e34",
+      "name": "羅馬尼亞硬拉",
+      "muscles": [
+        {
+          "muscle": "腿臀",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781365878974
+    },
+    {
+      "id": "8d4e4d9b-9c04-42a3-b9ee-36ffa24d0db9",
+      "name": "槌式彎舉（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "二頭",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "9738e226-fd21-4e54-a5b8-0de678364db4",
+      "name": "過頭三頭肌伸展（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "三頭",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781365367549
+    },
+    {
+      "id": "a1ade363-7eab-4bb9-b2e1-3445c6123ea7",
+      "name": "肩推（啞鈴）",
       "muscles": [
         {
           "muscle": "肩",
@@ -157,8 +278,30 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "f239fadb-3fc2-4cfe-9e36-1e6f49403292",
-      "name": "胸飛鳥 (機器)",
+      "id": "abbde7dd-278e-4ba8-b4c6-2a2a0f86a720",
+      "name": "腿屈伸（機器）",
+      "muscles": [
+        {
+          "muscle": "腿臀",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "af4bc2f9-cc40-4beb-a8a1-1e7b7dc0930e",
+      "name": "俯身划船（槓鈴）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "b7212335-963c-4a86-9abd-3614e3bdabe2",
+      "name": "上斜臥推（槓鈴）",
       "muscles": [
         {
           "muscle": "胸",
@@ -179,116 +322,6 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "025e32ac-929e-49a7-a76a-b4c28031b79a",
-      "name": "過頭三頭肌伸展 (滑輪)",
-      "muscles": [
-        {
-          "muscle": "三頭",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "4185b790-e7c3-45b4-b6c3-6a8a9a72b785",
-      "name": "肩推 (槓鈴)",
-      "muscles": [
-        {
-          "muscle": "肩",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "562a9462-05fd-43b7-afe8-2e487acef683",
-      "name": "臥推 (槓鈴)",
-      "muscles": [
-        {
-          "muscle": "胸",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "2fbac25e-fbb0-4edd-a6ea-d10f3692a1b3",
-      "name": "上斜臥推 (機器)",
-      "muscles": [
-        {
-          "muscle": "胸",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "09852e25-10d0-49f8-b9cf-3ca7cb675bf3",
-      "name": "反向飛鳥 (機器)",
-      "muscles": [
-        {
-          "muscle": "肩",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "5e1438a9-cba7-4ecf-a311-6a10bc070ba0",
-      "name": "蝴蝶機夾胸 (機器)",
-      "muscles": [
-        {
-          "muscle": "胸",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "f9668f1a-0971-4ff5-b9f1-6c1d99827e1c",
-      "name": "三頭肌伸展 (機器)",
-      "muscles": [
-        {
-          "muscle": "三頭",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "fad3fd3c-9b56-41bc-89c6-d64731eabae0",
-      "name": "三頭肌下壓 (滑輪)",
-      "muscles": [
-        {
-          "muscle": "三頭",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "102ea501-db99-4e31-bf21-c9b6fa8f2960",
-      "name": "引體向上 (輔助)",
-      "muscles": [
-        {
-          "muscle": "背",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "ecb2fe89-3eba-43c3-b238-4d9eb2b2ffe4",
-      "name": "坐姿划船 (滑輪)",
-      "muscles": [
-        {
-          "muscle": "背",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
       "id": "c98068e4-7629-4a10-9c62-851b9ade3c50",
       "name": "腿伸展",
       "muscles": [
@@ -300,63 +333,8 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "793e511e-58e7-4580-8e26-a9bb4a4bbdf1",
-      "name": "臀推 (槓鈴)",
-      "muscles": [
-        {
-          "muscle": "腿臀",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "26154708-c018-42c5-99b4-3f81083ff029",
-      "name": "牧師椅彎舉 (機器)",
-      "muscles": [
-        {
-          "muscle": "二頭",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "5597fee9-59e0-490a-b2aa-36ad5c9b6394",
-      "name": "上斜臥推 (啞鈴)",
-      "muscles": [
-        {
-          "muscle": "胸",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "14647ded-dbf9-4a6e-b8bc-b55203168d53",
-      "name": "坐姿窄握划船 (機器)",
-      "muscles": [
-        {
-          "muscle": "背",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "dce71b30-dc93-49af-bee2-55c77d45a49e",
-      "name": "二頭彎舉 (槓鈴)",
-      "muscles": [
-        {
-          "muscle": "二頭",
-          "weight": 1
-        }
-      ],
-      "createdAt": 1781338662648
-    },
-    {
-      "id": "a1ade363-7eab-4bb9-b2e1-3445c6123ea7",
-      "name": "肩推 (啞鈴)",
+      "id": "c9bbbb70-3b87-43d2-962a-34773b8fff48",
+      "name": "肩推（機器）",
       "muscles": [
         {
           "muscle": "肩",
@@ -366,8 +344,89 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "f1ebf0c8-35db-4d4b-b371-127e9d16dac1",
-      "name": "划船 (機器)",
+      "id": "cfd66000-e3a5-4b31-999a-2c19cbae4268",
+      "name": "臥推（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "胸",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781365320415
+    },
+    {
+      "id": "d1ea4818-d84a-445f-8835-99b48a06afa3",
+      "name": "硬拉",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        },
+        {
+          "muscle": "腿臀",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781356466795
+    },
+    {
+      "id": "d93e2c5b-1cec-4a73-980f-383cf0f2be85",
+      "name": "反向飛鳥（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781365595966
+    },
+    {
+      "id": "dac2bc74-bf5b-4d78-916b-5d186e41794c",
+      "name": "俯身划船（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781365746065
+    },
+    {
+      "id": "dce71b30-dc93-49af-bee2-55c77d45a49e",
+      "name": "二頭彎舉（槓鈴）",
+      "muscles": [
+        {
+          "muscle": "二頭",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "eae33992-5d5b-48a9-94a2-30f95dff142a",
+      "name": "上斜彎舉（啞鈴）",
+      "muscles": [
+        {
+          "muscle": "二頭",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "eb05fe5e-491a-4515-a587-5d7714e64167",
+      "name": "腿部外展",
+      "muscles": [
+        {
+          "muscle": "腿臀",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "ecb2fe89-3eba-43c3-b238-4d9eb2b2ffe4",
+      "name": "坐姿划船（滑輪）",
       "muscles": [
         {
           "muscle": "背",
@@ -377,11 +436,44 @@ export const SEED = {
       "createdAt": 1781338662648
     },
     {
-      "id": "af4bc2f9-cc40-4beb-a8a1-1e7b7dc0930e",
-      "name": "俯身划船 (槓鈴)",
+      "id": "f1ebf0c8-35db-4d4b-b371-127e9d16dac1",
+      "name": "划船（機器）",
       "muscles": [
         {
           "muscle": "背",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "f239fadb-3fc2-4cfe-9e36-1e6f49403292",
+      "name": "胸飛鳥（機器）",
+      "muscles": [
+        {
+          "muscle": "胸",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "f9668f1a-0971-4ff5-b9f1-6c1d99827e1c",
+      "name": "三頭肌伸展（機器）",
+      "muscles": [
+        {
+          "muscle": "三頭",
+          "weight": 1
+        }
+      ],
+      "createdAt": 1781338662648
+    },
+    {
+      "id": "fad3fd3c-9b56-41bc-89c6-d64731eabae0",
+      "name": "三頭肌下壓（滑輪）",
+      "muscles": [
+        {
+          "muscle": "三頭",
           "weight": 1
         }
       ],
